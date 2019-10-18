20fc9677fa51e4a067be0fa819103160f9dfc615
steven <steven@nenyah.com>
2019-10-18 17:06:33 +0800

    fix home page

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,11 +1,10 @@
 import lifecycle from '/utils/lifecycle';
-import animModal from '/utils/items';
 
 Page({
   ...lifecycle,
-  ...animModal.animOp,
+
   data: {
-    ...animModal.data,
+
     pageName: 'component/index',
     pageInfo: {
       pageId: 0,
@@ -20,23 +19,7 @@ Page({
           icon: '/image/view.png',
           title: '视图容器',
           entitle: 'View',
-          subs: [
-            {
-              title: '基础视图',
-              entitle: 'View',
-              page: 'view',
-            },
-            {
-              title: '滚动视图',
-              entitle: 'ScrollView',
-              page: 'scroll-view',
-            },
-            {
-              title: '滑动视图',
-              entitle: 'Swiper',
-              page: 'swiper',
-            },
-          ],
+      
         }
       ]
     }
